1b5d98525a7130396264bee9ce16a13dfac28e52
mnrqcmn3v <sebas101972@hotmail.com>
2023-05-27 12:59:14 -0500

    analisis2

---
+++ controllers/controlador.js
@@ -0,0 +1,11 @@
+let boton=document.getElementById("btnReporte")
+boton.addEventListener("click",function(evento){
+    evento.preventDefault()
+    let contenedor=document.getElementById("contenedor")
+    contenedor.classList.remove("d-none")
+
+    let nombreUsuario=document.getElementById("nombre").value
+    let mensaje=document.getElementById("mensaje")
+
+    mensaje.textContent=`Apreciado usuario:${nombreUsuario},hemos generado su reporte:`
+})
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from data.apartamentos import apartamento1,apartamento2
 import pandas as pd
+from helpers.crearTablasHTML import crearTabla
 
 #crear el dataframe
 
@@ -9,13 +10,27 @@ tabla3=pd.read_csv('./data/empleados.csv')
 
 # print(tabla1)
 # print(tabla2)
+print(tabla3)
 
-estadisticasAPT01=tabla1.describe()
-estadisticasAPT02=tabla2.describe()
-estadisticasEmpleados=tabla3.describe()
+#efectuando filtros con python
+#1 necisto definir una condicion logica
 
-print(estadisticasAPT01)
-print('\n')
-print(estadisticasAPT02)
-print('\n')
-print(estadisticasEmpleados)
+empleadosJovenesAnalistas1=tabla3.query('edad<28 and cargo=="analista1"')
+empladosSalariosBajo=tabla3.query('salario<5000000 and cargo=="analista2"')
+empleadosADespedir=tabla3.query('edad>=50 ')
+
+# estadisticasAPT01=tabla1.describe()
+# estadisticasAPT02=tabla2.describe()
+# estadisticasEmpleados=tabla3.describe()
+
+# print(estadisticasAPT01)
+# print('\n')
+# print(estadisticasAPT02)
+# print('\n')
+# print(estadisticasEmpleados)
+
+#2 crearmos la tabla html con el dataframe del filtro
+crearTabla(empleadosJovenesAnalistas1,"tablaJovenes")
+crearTabla(empladosSalariosBajo,"tablaBajoSalario")
+crearTabla(empleadosADespedir,"tablaoportunidaddemejora")
+
